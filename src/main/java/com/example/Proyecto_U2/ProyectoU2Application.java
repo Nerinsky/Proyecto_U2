602e4533af1402afafe5ee33fb89e9aa97c35ef6
Nerinsky <74480258+Nerinsky@users.noreply.github.com>
2020-11-15 07:27:20 -0600

    Add files via upload

---
+++ src/main/java/com/example/Proyecto_U2/ProyectoU2Application.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_U2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoU2Application.class, args);
+	}
+
+}
